7456c8adbda6554aa091f2c069bee1caade3ed1c
andmohiko <andmohiko@gmail.com>
2020-12-15 00:57:23 +0900

    fix name

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div>
       <logo />
       <h1 class="title">
-        smash-bros-fighting-recorder
+        smashlytics
       </h1>
       <h2 class="subtitle">
         smash bros fighting result recording app
