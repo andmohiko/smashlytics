1de27ce7f12ef24d91269f5eda84b7b34ad2abe9
andmohiko <andmohiko@gmail.com>
2020-12-25 05:29:36 +0900

    add page title in header

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="header">
-    <p class="title"><nuxt-link to="/">すましす</nuxt-link></p>
+    <p class="page-title">{{ pageTitle }}</p>
     <p class="user">{{ user.username }} @{{ user.userId }}</p>
     <nav class="nav">
       <nuxt-link to="/fighters">fighters</nuxt-link>
@@ -20,19 +20,57 @@ export default {
   },
   mounted() {
     this.user = this.$store.state.user
+  },
+  computed: {
+    routename() {
+      return this.$route.name
+    },
+    pageTitle() {
+      switch (this.routename) {
+        case 'records': {
+          return '戦績一覧'
+        }
+        case 'analytics': {
+          return '分析'
+        }
+        case 'mypage-id': {
+          return 'マイページ'
+        }
+        case 'edit': {
+          return 'プロフィール編集'
+        }
+        case 'fighters': {
+          return 'ファイター追加'
+        }
+        case 'login': {
+          return 'ログイン'
+        }
+        case 'signup': {
+          return 'ユーザ登録'
+        }
+        default: {
+          return 'すましす'
+        }
+      }
+    }
+  },
+  methods: {
+    route() {
+      console.log(this.$route.name)
+    }
   }
 }
 </script>
 
 <style scoped lang="scss">
 .header {
-  height: 80px;
+  height: 60px;
   width: 100%;
-  background-color: rgba(255, 255, 80, 0.9);
+  background-color: rgba(255, 255, 80, 0.8);
   position: fixed;
   top: 0;
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
   align-items: center;
 }
 .title {
@@ -40,9 +78,14 @@ export default {
   color: black;
   margin-left: 40px;
 }
+.page-title {
+  font-size: 24px;
+  font-weight: 600;
+  color: #4a5568d9;
+}
 .nav {
   display: flex;
-  width: 300px;
+  // width: 300px;
   justify-content: space-between;
   align-items: center;
   margin-right: 40px;
