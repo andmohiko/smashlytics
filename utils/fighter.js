bc5023e5a4f7440a82b61188ae782ffd9998a093
andmohiko <andmohiko@gmail.com>
2020-12-25 22:08:06 +0900

    add winning percentage

--- utils/fighter.js
+++ utils/fighter.js
@@ -6,3 +6,10 @@ export function jp2en(fighterJp, fighters) {
   const fighter = Object.values(fighters).filter(f => f.japanese === fighterJp)[0]
   return fighter.english
 }
+
+export function calcWinnigPercentage(records) {
+  const wins = records.filter(record => record.result).length
+  const loses = records.length - wins
+  const percentage = Math.round((wins/records.length)*100 * 10) / 10
+  return wins + '勝' + loses + '敗 勝率' + percentage + '%'
+}
