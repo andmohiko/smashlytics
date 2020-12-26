0212f50ba6f27cc7c1b7efb4f80003c0c0dbb334
andmohiko <andmohiko@gmail.com>
2020-12-26 18:24:20 +0900

    set cookie on login

--- pages/login.vue
+++ pages/login.vue
@@ -10,21 +10,7 @@
 </template>
 
 <script>
-// import { firebase } from '@/plugins/firebase'
-// import firebase from '@/plugins/firebase'
-
 export default {
-  data () {
-    return {
-      newUser: {
-        userId: '',
-        username: '',
-        twitterId: '',
-        email: '',
-        password: ''
-      }
-    }
-  },
   methods: {
     login() {
       this.$store.dispatch('loginGoogle')
