04c2ab4931453a4519ef1fbef5bf5110b2071bbb
andmohiko <andmohiko@gmail.com>
2020-12-26 18:24:55 +0900

    get cookie at load

--- plugins/setup.js
+++ plugins/setup.js
@@ -1,9 +1,20 @@
+import Cookies from "universal-cookie"
+import jwtDecode from "jwt-decode"
+
 export default async ({ store }) => {
   try {
-    const resultRecords = await store.dispatch('getRecords', 'andmohiko')
-    // console.log('resultRecords', resultRecords)
+    const cookie = new Cookies()
+    const auth_token = cookie.get('smash_access_token')
+    console.log('setupjs got token', Boolean(auth_token))
+    if (!auth_token) return
+
+    const decodedToken = jwtDecode(auth_token)
+    const uid = decodedToken['user_id']
+    console.log('setupjs decoded userId', uid)
+    store.commit('setUid', uid)
+    store.dispatch('isUser', uid)
+    
     const resultFighters = await store.dispatch('getFighters')
-    // console.log('resultFighters', resultFighters)
   } catch (error) {
     console.log('error in setup', error)
   }
