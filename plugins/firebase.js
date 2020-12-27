15a2770d9ffe9ae891e913d88b192b252467ff3b
rin1208 <sora1208sky@gmail.com>
2020-12-27 18:40:16 +0900

    DATABASE_URLの削除

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -6,7 +6,6 @@ import 'firebase/firestore'
 const fbConfig = {
   apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
   projectId: process.env.PROJECT_ID,
   storageBucket: process.env.STORAGE_BUCKET,
   messagingSenderId: process.env.MESSAGE_SENDER_ID,
