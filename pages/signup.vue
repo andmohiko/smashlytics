855dfa832ce9f458c41e2f46f8e1e5d6a574681b
andmohiko <andmohiko@gmail.com>
2021-01-02 06:03:52 +0900

    check unique userId

--- pages/signup.vue
+++ pages/signup.vue
@@ -54,8 +54,19 @@ export default {
         mainFighterId: ''
       },
       uid: '',
-      error: ''
+      error: '',
+      userIds: []
     }
+  },
+  async fetch() {
+    const db = firebase.firestore()
+    await db.collection('users')
+      .get()
+      .then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+          this.userIds.push(doc.id)
+        })
+      })
   },
   methods: {
     select() {
@@ -67,12 +78,21 @@ export default {
       this.user.userId = this.$refs.userId.input
       this.user.username = this.$refs.username.input
       this.user.twitterId = this.$refs.twitterId.input || null
-      if (this.user.twitterId && this.user.twitterId.slice(0,1) === '@') {
-        this.user.twitterId = this.user.twitterId.slice(1)
+      if (!authId) {
+        this.error = 'ログインできておりません。再度ログインしてください。'
+        return
       }
-      if (!authId || this.user.userId === '' || this.user.username === '') {
+      if (this.user.userId === '' || this.user.username === '') {
         this.error = 'ユーザIDとユーザ名を入力してください'
         return
+      }
+      console.log('this.userIds', this.userIds)
+      if (this.userIds.includes(this.user.userId)) {
+        this.error = '入力されたユーザIDは使用できません'
+        return
+      }
+      if (this.user.twitterId && this.user.twitterId.slice(0,1) === '@') {
+        this.user.twitterId = this.user.twitterId.slice(1)
       }
       const db = firebase.firestore()
       try {
