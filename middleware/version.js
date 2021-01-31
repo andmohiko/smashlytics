73d05f67111dd6d86c1af7ef9ff1df28589157e5
andmohiko <andmohiko@gmail.com>
2021-01-31 20:38:07 +0900

    refresh version at every page

--- middleware/version.js
+++ middleware/version.js
@@ -3,7 +3,7 @@ const db = firebase.firestore()
 
 export default async function({ app, store }) {
   // 未ログインは何もしない
-  if (!store.state.isLogin) return
+  // if (!store.state.isLogin) return
   // 最新バージョン取得から3分以内なら更新しない
   const now = (new Date()).getTime()
   const version = store.state.version
