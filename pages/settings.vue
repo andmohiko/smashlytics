441c27733415bbcdb7e38c0a9f358b9700254c9c
andmohiko <andmohiko@gmail.com>
2021-02-02 23:41:13 +0900

    add release note

--- pages/settings.vue
+++ pages/settings.vue
@@ -36,6 +36,18 @@
       <div class="bg-white shadow-md rounded px-8 pt-2 pb-6 mb-4 flex flex-col">
         <h2 class="text-xl mb-4">Release notes</h2>
         <div class="version text-left mb-4">
+          <h3 class="text-base">Ver 3.4.6</h3>
+          <ul class="text-sm">
+            <li>◎ステージ選択にカロスポケモンリーグを追加</li>
+          </ul>
+        </div>
+        <div class="version text-left mb-4">
+          <h3 class="text-base">Ver 3.4.5</h3>
+          <ul class="text-sm">
+            <li>◎ログインできない不具合を修正</li>
+          </ul>
+        </div>
+        <div class="version text-left mb-4">
           <h3 class="text-base">Ver 3.4.4</h3>
           <ul class="text-sm">
             <li>◎マイページに今週の世界戦闘力の変動を表示</li>
