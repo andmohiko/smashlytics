4f2063b12ddd317474d4fdf2648607a7fa53bc83
andmohiko <andmohiko@gmail.com>
2021-02-17 11:22:05 +0900

    fix coming soon icon color

--- components/Footer.vue
+++ components/Footer.vue
@@ -47,8 +47,8 @@
           <div class="menu-item">
             <div class="menu-icon">
               <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M11 3.05493C6.50005 3.55238 3 7.36745 3 12C3 16.9706 7.02944 21 12 21C16.6326 21 20.4476 17.5 20.9451 13H11V3.05493Z" stroke="#a8b1c2d9" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                <path d="M20.4878 8.99991H15V3.51221C17.5572 4.41604 19.5839 6.44276 20.4878 8.99991Z" stroke="#a8b1c2d9" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M11 3.05493C6.50005 3.55238 3 7.36745 3 12C3 16.9706 7.02944 21 12 21C16.6326 21 20.4476 17.5 20.9451 13H11V3.05493Z" stroke="#a8b1c289" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M20.4878 8.99991H15V3.51221C17.5572 4.41604 19.5839 6.44276 20.4878 8.99991Z" stroke="#a8b1c289" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
               </svg>
             </div>
             <span class="coming-soon">統計</span>
