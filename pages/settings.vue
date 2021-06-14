abdab04cfaa676fe61d9dd365c040c174475cde2
andmohiko <andmohiko@gmail.com>
2021-06-14 19:09:19 +0900

    add release note page

--- pages/settings.vue
+++ pages/settings.vue
@@ -28,11 +28,11 @@
       <div class="bg-white shadow-md rounded px-8 pt-2 pb-6 mb-4 flex flex-col">
         <h2 class="text-xl mb-4">専用部屋の機能</h2>
         <ul class="text-left">
-          <li>
-            <nuxt-link to="/arena">戦績の登録</nuxt-link>
+          <li class="pb-2">
+            ✏️<nuxt-link to="/arena">戦績の登録</nuxt-link>
           </li>
           <li>
-            <nuxt-link to="/arenaAnalyticsBeta">戦績の分析</nuxt-link>
+            🔍<nuxt-link to="/arenaAnalyticsBeta">戦績の分析</nuxt-link>
           </li>
         </ul>
       </div>
@@ -46,6 +46,13 @@
       </div>
       <div class="bg-white shadow-md rounded px-8 pt-2 pb-6 mb-4 flex flex-col">
         <h2 class="text-xl mb-4">Release notes</h2>
+        <div class="version text-left mb-4">
+          <h3 class="text-base">Ver 4.0.6</h3>
+          <ul class="text-sm">
+            <li>◎リリースノートを別ページにまとめました</li>
+            <li>◎その他軽微なデザイン修正</li>
+          </ul>
+        </div>
         <div class="version text-left mb-4">
           <h3 class="text-base">Ver 4.0.5</h3>
           <ul class="text-sm">
@@ -85,146 +92,12 @@
           </ul>
         </div>
         <div class="version text-left mb-4">
-          <h3 class="text-base">Ver 3.5.0</h3>
-          <ul class="text-sm">
-            <li>◎分析で日付を指定して期間を絞り込む機能を追加</li>
-          </ul>
-        </div>
-        <div class="version text-left mb-4">
-          <h3 class="text-base">Ver 3.4.6</h3>
-          <ul class="text-sm">
-            <li>◎ステージ選択にカロスポケモンリーグを追加</li>
-          </ul>
-        </div>
-        <div class="version text-left mb-4">
-          <h3 class="text-base">Ver 3.4.5</h3>
-          <ul class="text-sm">
-            <li>◎ログインできない不具合を修正</li>
-          </ul>
-        </div>
-        <div class="version text-left mb-4">
-          <h3 class="text-base">Ver 3.4.4</h3>
-          <ul class="text-sm">
-            <li>◎マイページに今週の世界戦闘力の変動を表示</li>
-            <li>◎マイページに各キャラの総合勝率を表示</li>
-          </ul>
-        </div>
-        <div class="version text-left mb-4">
-          <h3 class="text-base">Ver 3.4.3</h3>
-          <ul class="text-sm">
-            <li>◎エラー通知の追加</li>
-          </ul>
-        </div>
-        <div class="version text-left mb-4">
-          <h3 class="text-base">Ver 3.4.2</h3>
-          <ul class="text-sm">
-            <li>◎ラジオボタンのデザイン変更</li>
-            <li>◎専用部屋の分析で期間が絞り込まれない不具合を修正</li>
-          </ul>
-        </div>
-        <div class="version text-left mb-4">
-          <h3 class="text-base">Ver 3.4.1</h3>
-          <ul class="text-sm">
-            <li>◎専用部屋の戦績を編集できない不具合を修正</li>
-            <li>◎分析で期間を絞り込む選択肢に「本日」を追加</li>
-          </ul>
-        </div>
-        <div class="version text-left mb-4">
-          <h3 class="text-base">Ver 3.4.0</h3>
-          <ul class="text-sm">
-            <li>◎戦績登録時にストック数を入力欄を追加</li>
-            <li>◎その他デザインの修正</li>
-          </ul>
-        </div>
-        <div class="version text-left mb-4">
-          <h3 class="text-base">Ver 3.3.0</h3>
-          <ul class="text-sm">
-            <li>◎分析をより詳細に設定できる機能の追加</li>
-            <li>◎分析で自分のファイターで絞る機能の追加</li>
-            <li>◎その他デザインの修正</li>
-          </ul>
-        </div>
-        <div class="version text-left mb-4">
-          <h3 class="text-base">Ver 3.2.0</h3>
-          <ul class="text-sm">
-            <li>◎分析で類似ファイターをまとめる機能を追加</li>
-            <li>◎専門部屋の戦績登録で直前の相手の名前を直接選択できる機能を追加</li>
-          </ul>
-        </div>
-        <div class="version text-left mb-4">
-          <h3 class="text-base">Ver 3.1.0</h3>
-          <ul class="text-sm">
-            <li>◎専用部屋の分析機能(β版)を追加</li>
-          </ul>
-        </div>
-        <div class="version text-left mb-4">
-          <h3 class="text-base">Ver 3.0.0</h3>
-          <ul class="text-sm">
-            <li>◎専用部屋の戦績記録機能を追加</li>
-          </ul>
-        </div>
-        <div class="version text-left mb-4">
-          <h3 class="text-base">Ver 2.1.0</h3>
-          <ul class="text-sm">
-            <li>◎戦績編集時の勝率のずれを修正</li>
-          </ul>
-        </div>
-        <div class="version text-left mb-4">
-          <h3 class="text-base">Ver 2.0.0</h3>
-          <ul class="text-sm">
-            <li>開発者の大きな過ちを修正しました。</li>
-          </ul>
-        </div>
-        <div class="version text-left mb-4">
-          <h3 class="text-base">Ver 1.0.0</h3>
-          <ul class="text-sm">
-            <li>公開しました！！！！！</li>
-          </ul>
-        </div>
-        <div class="version text-left mb-4">
-          <h3 class="text-base">Ver 0.5.0</h3>
-          <ul class="text-sm">
-            <li>◎戦績の記録項目に連戦かどうかとVIPマッチかどうかを追加</li>
-            <li>◎プロフィールに主なプレイ時間・使用ボイチャを追加</li>
-          </ul>
-        </div>
-        <div class="version text-left mb-4">
-          <h3 class="text-base">Ver 0.4.0</h3>
-          <ul class="text-sm">
-            <li>◎プロフィールの公開</li>
-            <li>◎マイページに使用キャラの最新世界戦闘力を表示</li>
-            <li>◎日付/時間表示のバグ修正</li>
-            <li>◎プロフィールの編集機能</li>
-          </ul>
-        </div>
-        <div class="version text-left mb-4">
-          <h3 class="text-base">Ver 0.3.0</h3>
-          <ul class="text-sm">
-            <li>◎ステージごとの勝率の確認</li>
-            <li>◎分析の並べ替えで逆順を追加</li>
-            <li>◎ログイン状態の保持</li>
-          </ul>
-        </div>
-        <div class="version text-left mb-4">
-          <h3 class="text-base">Ver 0.2.0</h3>
-          <ul class="text-sm">
-            <li>◎戦績の編集・削除</li>
-            <li>◎戦績の一括登録</li>
-            <li>◎未ログインで戦績を記録できてしまうバグの修正</li>
-          </ul>
-        </div>
-        <div class="version text-left">
-          <h3 class="text-base">Ver 0.1.0</h3>
-          <ul class="text-sm">
-            <li>◎ログイン機能</li>
-            <li>◎戦績の登録</li>
-            <li>◎期間ごとの勝率の確認</li>
-          </ul>
+          Ver 4.0.0以前は<nuxt-link to="/release-notes">こちら</nuxt-link>
         </div>
       </div>
       <Button @onClick="logout" label="ログアウト" />
       <div class="flex justify-end text-sm text-gray-500 pt-6 pr-2">
-        Version 4.0.2
+        Version 4.0.6
       </div>
       <div class="copyright">
         <p>
